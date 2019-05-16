9aae951c8bf24e0ff91df4dd174077688fd5210f
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-16 20:42:37 +0100

    moved the deselect method to Tabs class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,25 +6,26 @@ class Link{
   }
 
   select (e) {
-    this.deselect()
     e.classList.add('tabs-link-selected');
     this.data= e.dataset.tab;
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     this.itemElement.classList.add('tabs-item-selected')
-  }
-
-  deselect () {
-    const items = document.querySelectorAll('.tabs-link')
-    items.forEach(item=>item.classList.remove('tabs-link-selected'));
-    const tabLink = document.querySelectorAll('.tabs-item');
-    tabLink.forEach(item=> item.classList.remove('tabs-item-selected'))
   }
 }
 
 class Tabs {
   constructor(link) {
       this.link = link;
-      this.link.addEventListener('click', (e)=> new Link(e.target))
+      this.link.addEventListener('click', (e)=> {
+        this.deselect()
+        new Link(e.target)
+      })
+  }
+  deselect () {
+    const items = document.querySelectorAll('.tabs-link')
+    items.forEach(item=>item.classList.remove('tabs-link-selected'));
+    const tabLink = document.querySelectorAll('.tabs-item');
+    tabLink.forEach(item=> item.classList.remove('tabs-item-selected'))
   }
 }
 
