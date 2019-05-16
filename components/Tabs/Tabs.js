4788c402ac1c060b1fc2ffdcff0b67345a7ab872
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-16 14:14:51 +0100

    instatianted links to the class TabLink -completed Tablink constructor functionh -completed select method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,30 +3,36 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+      this.element = element
     // Get the custom data attribute on the Link
     // this.data;
-    
+      this.data = this.element.dataset.tab
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+      this.itemElement = document.querySelector(`div[data-tab='${this.data}']`)
+      console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+      this.tabItem = new TabItem(this.itemElement);
+      console.log(this.tabItem.element)
     // Add a click event listener on this instance, calling the select method on click
-
+      this.tabItem.element.addEventListener('click', (e)=>this.select())
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
+      const links = document.querySelectorAll('.tabs-link')
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
+    console.log(Array.from(links))
+      Array.from(links).forEach(tab=> {
+        console.log(tab)
+        tab.classList.remove('tabs-link-selected')
+      })
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+      this.element.classList.add('tabs-link-selected')
     // Call the select method on the item associated with this link
 
   }
@@ -35,7 +41,7 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element= element;
   }
 
   select() {
@@ -59,4 +65,5 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+const links = document.querySelectorAll('.tabs-link');
+links.forEach(link=> new TabLink(link))
